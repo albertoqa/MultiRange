8d5bcaf346f31d738c7184b0be4913a0f6c54086
Alberto Quesada <qa.alberto@gmail.com>
2016-12-10 15:15:42 +0100

    Sample scene showing a multi range control

---
+++ src/main/java/sample/MultiRangeSample.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import multirange.MultiRange;
+
+/**
+ * Created by alberto on 10/12/2016.
+ */
+public class MultiRangeSample extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(new MultiRange()));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
